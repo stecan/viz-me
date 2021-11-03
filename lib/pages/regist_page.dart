db1985486618bc4ea83874c9e1fe315ef4d55e58
ytskmt14 <sorigeeeen@gmail.com>
2021-11-03 14:15:25 +0900

    add regist page

--- lib/pages/regist_page.dart
+++ lib/pages/regist_page.dart
@@ -1,22 +1,57 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'dummy_page.dart';
+import 'login_page.dart';
 
 class Regist extends StatefulWidget {
   Regist({Key? key, required this.title}) : super(key: key);
-
   final String title;
-
   @override
   _RegistState createState() => _RegistState();
 }
 
 class _RegistState extends State<Regist> {
-  int _counter = 100;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  String email='';
+  String password='';
+
+  String infoText='';
+
+  void _signUpWithEmailAndPassword() async {
+    try {
+      // メール・パスワードでユーザ登録
+      final FirebaseAuth _auth = FirebaseAuth.instance;
+      await _auth.createUserWithEmailAndPassword(
+          email: email,
+          password: password
+      );
+      // ユーザ登録に成功した場合
+      // TOP画面に遷移
+      await Navigator
+          .of(context)
+          .pushReplacement(
+          MaterialPageRoute(builder: (context) {
+            return Dummy(title: '',);
+          })
+      );
+    } on FirebaseAuthException catch(e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+      setState(() {
+        infoText = "登録に失敗しました。：${e.toString()}";
+      });
+    } catch (e) {
+      print(e);
+      setState(() {
+        infoText = "登録に失敗しました。：${e.toString()}";
+      });
+    }
+
   }
 
   @override
@@ -26,38 +61,64 @@ class _RegistState extends State<Regist> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              obscureText:false,
-              decoration: const InputDecoration(
-                hintText: 'Type your email address',
-                labelText: 'email address',
+        child: Container(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                obscureText: false,
+                decoration: const InputDecoration(
+                  hintText: 'Type your email address',
+                  labelText: 'email address',
+                ),
+                onChanged: (String value) {
+                  setState(() {
+                    email = value;
+                  });
+                },
               ),
-            ),
-            TextField(
-              obscureText:true,
-              decoration: const InputDecoration(
-                hintText: 'Type your password',
-                labelText: 'password',
+              TextField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: 'Type your password',
+                  labelText: 'password',
+                ),
+                onChanged: (String value) {
+                  setState(() {
+                    password = value;
+                  });
+                },
               ),
-            ),
-            FlatButton(
-              onPressed: _incrementCounter,
-              color: Colors.blue,
-              child: Text(
-                '登録',
+              Container(
+                padding: EdgeInsets.all(8),
+                child: Text(infoText),
               ),
-            ),
-            FlatButton(
-              onPressed: _incrementCounter,
-              color: Colors.blue,
-              child: Text(
-                'キャンセル',
+              Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                  child: Text('Sign Up',),
+                  onPressed: _signUpWithEmailAndPassword,
+                ),
               ),
-            ),
-          ],
+              Container(
+                width: double.infinity,
+                child: TextButton(
+                  child: Text('アカウントをお持ちの方はこちら',),
+                  onPressed: () {
+                    print('Go To Loginボタンが押されました。');
+                    Navigator
+                        .of(context)
+                        .pushReplacement(
+                        MaterialPageRoute(builder: (context) {
+                          return Login(title: 'ログイン',);
+                        })
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
